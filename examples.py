e3c49d549023dbe147327f3aebd57cd85e86e79c
Joe Stubbs <jstubbs@tacc.utexas.edu>
2016-09-08 17:30:32 -0500

    Example curl

--- examples.py
+++ examples.py
@@ -12,3 +12,4 @@ cl = Client('file://{}/UserAdmin-am19-staging.wsdl'.format(os.getcwd()))
 cl.set_options(headers={'Authorization': 'Basic {}'.format(userpass.decode('utf-8'))})
 cl.service.listUsers(filter='', limit=100)
 
+# curl -H "X-Jwt-Assertion-AGAVE-PROD: $jwt" localhost:5000/admin/jwt
